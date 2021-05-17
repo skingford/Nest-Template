831b9657387610987f3151fd4d84ac60674718bb
SteveKingFord <332312568@qq.com>
2021-05-17 15:54:25 +0800

    feat: 创建共享模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
+/*
+ * @Author: kingford
+ * @Date: 2021-05-17 14:04:10
+ * @LastEditTime: 2021-05-17 15:53:53
+ */
+import { SharedModule } from './shared/shared.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+        SharedModule, ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/shared/shared.module.ts
@@ -0,0 +1,15 @@
+/*
+ * @Author: kingford
+ * @Date: 2021-05-17 15:53:50
+ * @LastEditTime: 2021-05-17 15:53:54
+ */
+
+
+import { Module } from '@nestjs/common';
+
+@Module({
+    imports: [],
+    controllers: [],
+    providers: [],
+})
+export class SharedModule {}
