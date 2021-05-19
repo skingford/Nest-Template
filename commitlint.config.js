daa932b2f4001c4c53628168f817973e3395e4fb
kingford <332312568@qq.com>
2021-05-19 22:42:21 +0800

    ci: 项目整理

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,22 @@
-/*
- * @Author: kingford
- * @Date: 2021-05-14 11:52:02
- * @LastEditTime: 2021-05-14 14:59:14
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['upd', 'feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
+    ],
+  },
+};
+
+/**
+ * https://segmentfault.com/a/1190000017790694
+ * upd: 更新
+ * feat: 新增
+ * fix: 修复
+ * docs: 文档
+ * style: 样式
+ * refactor: 重构代码
+ * test: 单元测试
+ * chore: 构建过程或辅助工具的变动
  */
-module.exports = { extends: ['@commitlint/config-conventional'] };
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,66 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
+
 import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { ConfigModule, ConfigService } from 'nestjs-config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AdminModule } from './modules/admin/admin.module';
+import { FrontModule } from './modules/front/front.module';
+import { CommonModule } from './modules/common/common.module';
+import { SharedModule } from './modules/shared/shared.module';
 
-import { CatsModule } from './cats/cats.module';
-import { SharedModule } from './shared/shared.module';
-import { ErrorsInterceptor } from './common/errors.interceptor';
-import { Cats } from './cats/cats.entity';
+import { LoggingInterceptor } from './interceptors/logging/logging.interceptor';
+import { ValidationPipe } from './pipe/validation/validation.pipe';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'nest_demo',
-      entities: [Cats],
-      synchronize: true,
+    // 配置加载配置文件
+    ConfigModule.load(path.resolve(__dirname, 'config', '**/!(*.d).{ts,js}'), {
+      modifyConfigName: (name: string) => name.replace('.config', ''),
     }),
-
-    CatsModule,
+    // mysql的连接
+    TypeOrmModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        type: config.get('database.type'),
+        host: config.get('database.host'),
+        port: config.get('database.port'),
+        username: config.get('database.username'),
+        password: config.get('database.password'),
+        database: config.get('database.database'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        logging: config.get('database.logging'),
+        synchronize: true, // 同步数据库
+        timezone: '+08:00', // 东八区
+        cache: {
+          duration: 60000, // 1分钟的缓存
+        },
+        extra: {
+          poolMax: 32,
+          poolMin: 16,
+          queueTimeout: 60000,
+          pollPingInterval: 60, // 每隔60秒连接
+          pollTimeout: 60, // 连接有效60秒
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    AdminModule,
+    FrontModule,
+    CommonModule,
     SharedModule,
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [
     {
       provide: APP_INTERCEPTOR,
-      useClass: ErrorsInterceptor,
+      useClass: LoggingInterceptor,
     },
-    AppService,
+    // 全局使用管道(数据校验)
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
   ],
 })
 export class AppModule {}
