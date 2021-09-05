a204647cecd6a11bd9d21e01934a9b021b4ff193
SteveKingFord <332312568@qq.com>
2021-09-05 13:17:21 +0800

    feat: posts dto

---
+++ src/posts/dto/index.ts
@@ -0,0 +1,16 @@
+/*
+ * @Author: kingford
+ * @Date: 2021-09-05 13:04:59
+ * @LastEditTime: 2021-09-05 13:13:56
+ */
+export class CreateDto {
+  name: string;
+  age: number;
+  breed: string;
+}
+
+export class UpdateDto {
+  name: string;
+  age: number;
+  breed: string;
+}
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,13 +1,23 @@
 /*
  * @Author: kingford
  * @Date: 2021-09-05 09:59:23
- * @LastEditTime: 2021-09-05 11:21:20
+ * @LastEditTime: 2021-09-05 13:13:47
  */
 /*
 https://docs.nestjs.com/controllers#controllers
 */
 
-import { Controller, Get, Post, Param, HttpCode } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  HttpCode,
+  Body,
+} from '@nestjs/common';
+import { CreateDto, UpdateDto } from './dto';
 
 @Controller('posts')
 export class PostsController {
@@ -23,7 +33,19 @@ export class PostsController {
   }
 
   @Post()
-  create(): string {
+  create(@Body() createDto: CreateDto): string {
+    console.log(createDto);
     return 'This action adds a new post';
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateDto: UpdateDto) {
+    console.log(updateDto);
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
   }
 }
