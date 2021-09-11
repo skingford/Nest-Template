8767c78cfa0ae74ffc4288fcdaffcf72e0f2e7b6
kingford <32887862+skingford@users.noreply.github.com>
2021-09-11 15:24:36 +0800

    feat: 添加属性

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 <!--
  * @Author: kingford
  * @Date: 2021-09-03 08:42:11
- * @LastEditTime: 2021-09-06 08:54:38
+ * @LastEditTime: 2021-09-11 15:17:09
 -->
+
+# 小计
 
 ## Delete `␍`eslint(prettier/prettier)
 
@@ -10,3 +12,9 @@
 git config --global core.autocrlf false
 
 ```
+
+## 功能目录
+
+1. jwt 授权
+
+2. role 角色鉴权
--- src/modules/auth/dto/LoginDto.ts
+++ src/modules/auth/dto/LoginDto.ts
@@ -1,12 +1,29 @@
-import { IsString } from 'class-validator';
+/*
+ * @Author: kingford
+ * @Date: 2021-09-11 08:51:10
+ * @LastEditTime: 2021-09-11 15:21:58
+ */
+import { IsString, IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
-  @ApiProperty({ type: String })
-  @IsString()
+  @ApiProperty({
+    type: String,
+    required: true,
+    description: '用户名',
+    default: 'admin',
+  })
+  @IsNotEmpty({ message: '用户名不能为空' })
+  @IsString({ message: '真实姓名必须是 String 类型' })
   username: string;
 
-  @ApiProperty({ type: String })
-  @IsString()
+  @ApiProperty({
+    type: String,
+    required: true,
+    description: '密码',
+    default: 'admin',
+  })
+  @IsNotEmpty({ message: '密码不能为空' })
+  @IsString({ message: '真实姓名必须是 String 类型' })
   password: string;
 }
