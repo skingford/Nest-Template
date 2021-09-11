52de0e37c7c08d715979c09c8be951180aa8e7aa
SteveKingFord <332312568@qq.com>
2021-09-12 01:13:27 +0800

    feat: 出参日志

--- src/common/interceptors/transform.interceptor.ts
+++ src/common/interceptors/transform.interceptor.ts
@@ -1,27 +1,45 @@
 /*
  * @Author: kingford
  * @Date: 2021-09-08 01:10:06
- * @LastEditTime: 2021-09-08 01:10:06
+ * @LastEditTime: 2021-09-12 01:12:11
  */
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Logger } from '@/utils/log4js';
 
 export interface Response<T> {
   data: T;
 }
 
+// 出参处理
 export class TransformInterceptor<T>
   implements NestInterceptor<T, Response<T>>
 {
-  intercept(context: ExecutionContext, next: CallHandler<T>) {
-    console.log(context);
+  intercept(context: ExecutionContext, next: CallHandler<T>): Observable<any> {
+    console.log('intercept.context:', context);
+    const req = context.getArgByIndex(1).req;
 
     return next.handle().pipe(
-      map((data) => ({
-        code: 200,
-        message: 'OK',
-        data,
-      })),
+      map((data) => {
+        console.log('intercept.data:', data);
+
+        const logFormat = ` <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        Request original url: ${req.originalUrl}
+        Method: ${req.method}
+        IP: ${req.ip}
+        User: ${JSON.stringify(req.user)}
+        Response data:\n ${JSON.stringify(data)}
+        <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<`;
+        Logger.info(logFormat);
+        Logger.access(logFormat);
+
+        return {
+          code: 200,
+          message: 'OK',
+          data,
+        };
+      }),
     );
   }
 }
--- src/config/setup/setupGlobalInterceptors.ts
+++ src/config/setup/setupGlobalInterceptors.ts
@@ -1,17 +1,11 @@
 /*
  * @Author: kingford
  * @Date: 2021-09-08 01:12:45
- * @LastEditTime: 2021-09-11 17:57:54
+ * @LastEditTime: 2021-09-12 01:11:41
  */
 import type { INestApplication } from '@nestjs/common';
 import { TransformInterceptor } from '@/common/interceptors/transform.interceptor';
-import { LogInterceptor } from '@/common/interceptors/log.interceptor';
-import { ReportLogger } from '@/modules/log/ReportLogger';
 
 export function setupGlobalInterceptors(app: INestApplication) {
-  const reportLogger = new ReportLogger();
-  app.useGlobalInterceptors(
-    new TransformInterceptor(),
-    new LogInterceptor(reportLogger),
-  );
+  app.useGlobalInterceptors(new TransformInterceptor());
 }
