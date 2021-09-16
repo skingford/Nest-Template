105ecb02ed0840e3af042bce8af0e7d5f11dff7b
kingford <32887862+skingford@users.noreply.github.com>
2021-09-16 09:31:56 +0800

    feat: 主键 -> uuid

--- src/modules/posts/entities/posts.entity.ts
+++ src/modules/posts/entities/posts.entity.ts
@@ -1,15 +1,15 @@
 /*
  * @Author: kingford
  * @Date: 2021-09-08 01:02:27
- * @LastEditTime: 2021-09-09 09:04:29
+ * @LastEditTime: 2021-09-16 09:28:45
  */
 import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('posts')
 export class PostEntity {
-  @ApiProperty({ description: '自增 id' })
-  @PrimaryGeneratedColumn()
+  @ApiProperty({ description: 'uuid' })
+  @PrimaryGeneratedColumn('uuid')
   id: number;
 
   @ApiProperty({ description: '标题' })
