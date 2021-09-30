735a94c2f18b838d7018e74994e144944e9f06db
kingford <32887862+skingford@users.noreply.github.com>
2021-09-30 20:32:35 +0800

    feat: 添加orm配置注释

--- src/config/setup/setupORM.ts
+++ src/config/setup/setupORM.ts
@@ -1,12 +1,13 @@
 /*
  * @Author: kingford
  * @Date: 2021-09-30 20:25:34
- * @LastEditTime: 2021-09-30 20:29:14
+ * @LastEditTime: 2021-09-30 20:31:57
  */
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getConnectionOptions } from 'typeorm';
 
 export function setupTypeORM() {
+  // 添加ormconfig.json外的配置
   TypeOrmModule.forRootAsync({
     useFactory: async () =>
       Object.assign(await getConnectionOptions(), {
