d6ccb2111a733146b0a751693303b2f6eaf8a137
kingford <32887862+skingford@users.noreply.github.com>
2021-09-30 20:37:59 +0800

    feat: 修改环境文件

--- src/config/hooks/useEnvConfig.ts
+++ src/config/hooks/useEnvConfig.ts
@@ -1,9 +1,9 @@
 /*
  * @Author: kingford
  * @Date: 2021-09-08 00:40:25
- * @LastEditTime: 2021-09-22 09:13:02
+ * @LastEditTime: 2021-09-30 20:37:34
  */
-import { APP_KEY, REDIS_KEY, WECHAT_MINI_KEY, DB_KEY } from '../constant';
+import { APP_KEY, REDIS_KEY, WECHAT_MINI_KEY } from '../constant';
 
 export const useEnvConfig = () => {
   // env 读取的是环境变量多为字符串
@@ -12,15 +12,6 @@ export const useEnvConfig = () => {
   return {
     [APP_KEY]: {
       port: env.HOST_PORT || 3000,
-    },
-    [DB_KEY]: {
-      type: env.TYPEORM_CONNECTION,
-      database: env.TYPEORM_DATABASE,
-      host: env.TYPEORM_HOST,
-      port: parseInt(env.TYPEORM_PORT, 10) || 3306,
-      username: env.TYPEORM_USERNAME,
-      password: env.TYPEORM_PASSWORD,
-      synchronize: env.TYPEORM_SYNC === 'true' ? true : false,
     },
     [REDIS_KEY]: {
       host: env.REDIS_HOST,
