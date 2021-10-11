fff26b1048a81f10abe58730b87717b0afadcae3
kingford <32887862+skingford@users.noreply.github.com>
2021-10-11 20:22:36 +0800

    chore: tnwx

--- src/modules/wechat/mini/user/user.service.ts
+++ src/modules/wechat/mini/user/user.service.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: kingford
  * @Date: 2021-09-17 12:35:15
- * @LastEditTime: 2021-09-19 10:17:51
+ * @LastEditTime: 2021-10-11 20:21:14
  */
 import {
   Injectable,
@@ -17,6 +17,18 @@ import { CreateMiniUserDto, UpdateMiniUserDto } from './dto';
 import { defHttp } from '@/utils/http';
 import { Logger } from '@/utils/log';
 import { WECHAT_MINI_KEY } from '@/config';
+
+import {
+  WeChat,
+  ApiConfigKit,
+  QyApiConfigKit,
+  AccessToken,
+  QyAccessTokenApi,
+  Kits,
+  HttpKit,
+  MiniProgramApi,
+  ApiConfig,
+} from 'tnwx';
 
 @Injectable()
 export class UserService {
@@ -68,7 +80,10 @@ export class UserService {
   // 微信用户登录
   async login(code: string): Promise<WechatMiniUser> {
     // 1. 获取微信授权信息
-    const wxSession = await this.getWxSession(code);
+    //const wxSession = await this.getWxSession(code);
+    const wxSession = await this.code2Session(code);
+
+    console.log('tn.info:', wxSession);
 
     // 2. 通过openid判断用户是否注册，已注册直接返回用户信息，否则注册用户
     const { openid } = wxSession;
@@ -97,6 +112,31 @@ export class UserService {
       Logger.error('获取微信openId失败：' + msg);
       throw new HttpException(msg, HttpStatus.OK);
     }
+    return wxSession;
+  }
+
+  async code2Session(code: string): Promise<CreateMiniUserDto> {
+    const config = this.config.get(WECHAT_MINI_KEY);
+    const { appid, secret } = config;
+
+    const devApiConfig = new ApiConfig(appid, secret, 'jinwechatsecrecttoken');
+
+    // 微信公众号、微信小程序、微信小游戏 支持多应用
+    ApiConfigKit.putApiConfig(devApiConfig);
+
+    // 开启开发模式,方便调试
+    ApiConfigKit.devMode = true;
+
+    // 设置当前应用
+    ApiConfigKit.setCurrentAppId(devApiConfig.getAppId);
+
+    const wxSession = await MiniProgramApi.code2Session(
+      ApiConfigKit.getAppId,
+      ApiConfigKit.getApiConfig.getAppScrect,
+      code,
+    );
+
+    console.log('wxSession:', wxSession);
     return wxSession;
   }
 }
